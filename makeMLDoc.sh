e4b834b7d922fada1028bf76eeaf81811431cff0
shivam <shivams2799@gmail.com>
2022-05-24 15:36:39 +0000

    regenerated

--- makeMLDoc.sh
+++ makeMLDoc.sh
@@ -38,7 +38,7 @@ ecldoc --iroot ~/source/GNN --oroot ~/docs/GNN --eclcc=" -I ~/source" --exdocpat
 ecldoc --iroot ~/source/dbscan --oroot ~/docs/dbscan --eclcc=" -I ~/source" --exdocpaths ~/docs/ML_Core/xml --format "html,pdf" --exclude "internal/*.ecl,tests/*.ecl,tests/datasets/*.ecl" --hideInternal
 
 #HPCC_Causality
-ecldoc --iroot ~/source/HPCC_Causality --oroot ~/docs/HPCC_Causality --eclcc=" -I ~/source" --exdocpaths ~/docs/ML_Core/xml --format "html,pdf" --exclude "internal/*.ecl,Tests/*.ecl,Tests/Causality/*.ecl,Tests/Probability/*.ecl,Tests/Synth/*.ecl" --hideInternal
+ecldoc --iroot ~/source/HPCC_Causality --oroot ~/docs/HPCC_Causality --eclcc=" -I ~/source" --exdocpaths ~/docs/ML_Core/xml --format "html,pdf" --exclude "internal/*.ecl,Tests/Causality/*.ecl,Tests/Probability/*.ecl,Tests/Synth/*.ecl" --hideInternal
 
 # Now we copy the resulting index.pdf files to files in our publish area named correctly for each bundle.
 
@@ -53,4 +53,4 @@ cp -f ~/docs/KMeans/tex/index.pdf ~/share/Publish/KMeans.pdf
 cp -f ~/docs/TextVectors/tex/index.pdf ~/share/Publish/TextVectors.pdf
 cp -f ~/docs/GNN/tex/index.pdf ~/share/Publish/GNN.pdf
 cp -f ~/docs/dbscan/tex/index.pdf ~/share/Publish/dbscan.pdf
-cp -f ~/docs/dbscan/tex/index.pdf ~/share/Publish/HPCC_Causality.pdf
+cp -f ~/docs/HPCC_Causality/tex/index.pdf ~/share/Publish/HPCC_Causality.pdf
