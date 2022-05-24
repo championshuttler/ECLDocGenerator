a72320bef95f719d94a3c5b89ff10d3e432d30fb
shivam <shivams2799@gmail.com>
2022-05-24 17:27:42 +0000

    setup to host at pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'ecl-doc',
+    packages = ['ecl-doc'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/championshuttler/ECLDocGenerator',
+    download_url = 'https://github.com/championshuttler/ECLDocGenerator/archive/refs/tags/1.0.0.tar.gz',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
